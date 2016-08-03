4b4baa690e6e3ad45be102cf92b95fe6da31cdfa
jorge97710 <jazmitia_10@hotmail.com>
2016-08-03 15:18:44 -0600

    se agregan subrutinas
    Se agregan las subrutinas de minimo, maximo, y promedio en punto
    flotante (se utilizaron referencias de las presentaciones de blackboard)

--- main.s
+++ main.s
@@ -1,5 +1,8 @@
 	/* ***************************************************************** 
    main random.s
+   archivo main para el laboratorio
+   JORGE AZMITIA 15202
+   JULIO BARAHONA 14 
    
    ***************************************************************** */
 
@@ -8,11 +11,7 @@
 .data
 .align 2
 
-datoPromedio:	.word	0 @Almacena el promedio de los dos numeros
-formatop:		.asciz	"El promedio de los numeros es: %d  \n"
 datoIng:		.word	0 @Dato ingresado por el usuario
-dato2:			.word	5
-
 formatos:		.asciz	"%d"
 stringIngreso:	.asciz	"Ingrese la semilla para generar los numeros:"
 bienvenida:		.asciz	"Bienvenid@! Este programa calcula numeros pseudo aleatorios tomando una base ingresada por el usuario"
@@ -37,7 +36,6 @@ EORMI  r0,r0,#1<<31-14
 .func main
 
 main:
-	stmfd sp!,{lr}
 	
 	@Bienvenida al programa
 	ldr r0, =bienvenida
@@ -59,25 +57,30 @@ main:
 	ldr r0,=error
 	bl puts
 	b ingresoDatos
-
-
-
 	
-	
-	
-	@Calculo y despliegue del promedio
-@---------------------------------- ENTRA A SUBRUTINA------------------------------------------------
+@---------------------------------- ENTRA A SUBRUTINA lsfr------------------------------------------------
 	calculo:
+	ldr r0,=vector
+	mov r1,#4194304
+	mov r2,=semilla
+	mov r2,[r2]
+	bl lsfr
+@---------------------------------- ENTRA A SUBRUTINA min------------------------------------------------
+    ldr r0,=vector
+	mov r1,#4194304
+	bl min
 
-ldr r0,=vector
-mov r1,#4194304
-mov r2,=semilla
-mov r2,[r2]
-bl lsfr
+@---------------------------------- ENTRA A SUBRUTINA max------------------------------------------------
+	ldr r0,=vector
+	mov r1,#4194304
+	bl max
 
+@---------------------------------- ENTRA A SUBRUTINA avg------------------------------------------------
+	ldr r0,=vector
+	mov r1,#4194304
+	bl avg
 
 
 	
-		
 	MOV R7, #1				@ Exit Syscall
 	SWI 0
--- subrutinas.s
+++ subrutinas.s
@@ -1,29 +1,15 @@
 /* ***************************************************************** 
    subrutinas
-   Subrutina que calcula el promedio de dos numeros
+   Subrutina que calcula los random, el maximo, el minimo, normaliza el vector, y lo imprime
    
-   Uso de registros:
-   r0: envio de parametros para subrutinas promedio, printf, scanf, puts
-   r1: envio de parametros para subrutinas promedio, printf, scanf
+  
    ***************************************************************** */
-.text
-.align 2
 
-@Subrutina que calcula el promedio de dos numeros y devuelve el promedio 
-@Parametros de entrada:
-@r0: numero 1
-@r1: numero 2
 
-@Parametros de salida
-@r0: promedio
-
-.global promedio
-
-promedio:
-	push {lr}
-	add r0,r0,r1
-	lsr r0,#1	@division entre 2
-	pop {pc}
+.global lsfr
+.global min
+.global max
+.global avg
  
 @Comienzo subrutina lsfr
 lsfr:
@@ -39,33 +25,109 @@ ciclo:
 and r4,r2,#1
 lsr r2,r2 #1
 
-
-
-
-
-
 bne
 
-
-
-
 	pop {pc}
+	
+	
 @Comienzo subrutina min
 min:
 push {lr}
+	@en r0 viene la direccion del vector que se usara para recorrerlo
+	@en r1 se guarda el valor actual del vector
+	@en r2 es el minimo temporal
+	@en s2 el temporal
+	@en r3 va el contador para ver todas las posiciones del vector
+	@en r4 esta el tamaño del vector, que se pasa de r1
+	mov r2,#0
+	mov r3,#0
+	mov r4,r1
+	vmov s2,#0
 	
+	ciclominimo:
+	vldr s0,[r0],#1 @va pasando en todas las posiciones del vector
+	vcmp.F32 s0,s2  @lo mismo que abajo solo que para registros S
+	vmrs APSR_nzcv, FPSCR @banderas
+	@cmp r2,r1      @compara el valor actual del vector con el temporal, que inicia siendo 0 
+	vmovlt s2,s0
+	vmov s0,#0     @limpia el actual 
+	@movgt r2,r1    @si el temporal es mayor que el actual del vector, pasa el del vector al temporal minimo 
+	add r3,r3,#1   @aumenta el contador
+	cmp r3,r4 @recorre las 2^20 posiciones del vector 
+	blt ciclominimo
+	
+	@retorna en r0 el valor minimo de el vector 
+	
+	@convertimos el s2 a un D para luego convertirlo en un r (esto me da duda, pero lo tomo de las presentaciones que nos han dado )
+	vcvt.F64.F32 d9,s2
+	vcvt.S32.d64 r2,d9	
+	mov r0,r2 
 	pop {pc}
+	
+	
 @Comienzo subrutina max
 max:
 push {lr}
+	@en r0 viene la direccion del vector que se usara para recorrerlo
+	@en r1 se guarda el valor actual del vector
+	@en r2 es el minimo temporal
+	@en s2 el temporal
+	@en r3 va el contador para ver todas las posiciones del vector
+	@en r4 esta el tamaño del vector, que se pasa de r1
+	mov r2,#0
+	mov r3,#0
+	mov r4,r1
+	vmov s2,#0
 	
+	ciclomax:
+	vldr s0,[r0],#1 @va pasando en todas las posiciones del vector
+	vcmp.F32 s0,s2  @lo mismo que abajo solo que para registros S
+	vmrs APSR_nzcv, FPSCR @banderas
+	@cmp r2,r1      @compara el valor actual del vector con el temporal, que inicia siendo 0 
+	vmovgt s2,s0
+	vmov s0,#0     @limpia el actual 
+	@movgt r2,r1    @si el temporal es menor que el actual del vector, pasa el del vector al temporal maximo 
+	add r3,r3,#1   @aumenta el contador
+	cmp r3,r4 @recorre las 2^20 posiciones del vector 
+	blt ciclomax
+	
+	@retorna en r0 el valor minimo de el vector 
+	
+	@convertimos el s2 a un D para luego convertirlo en un r (esto me da duda, pero lo tomo de las presentaciones que nos han dado )
+	vcvt.F64.F32 d9,s2
+	vcvt.S32.d64 r2,d9	
+	mov r0,r2 
 	pop {pc}
-@Comienzo subrutina avg
+	
+	
+	@Comienzo subrutina avg
 avg:
 push {lr}
+	@en r0 viene la direccion del vector que se usara para recorrerlo
+	@en r1 se guarda el valor actual del vector
+	@en r2 es la suma temporal
+	@en s2 el suma temporal
+	@en r3 va el contador para ver todas las posiciones del vector
+	@en r4 esta el tamaño del vector, que se pasa de r1
+	mov r2,#0
+	mov r3,#0
+	mov r4,r1
+	vmov s2,#0
 	
+	ciclosuma:
+	vldr s0,[r0],#1 @va pasando en todas las posiciones del vector
+	vadd.F32 s0,s0,s2 
+	add r3,r3,#1   @aumenta el contador
+	cmp r3,r4 @recorre las 2^20 posiciones del vector 
+	blt ciclosuma
+	@retorna en r0 el valor minimo de el vector 
+	@convertimos el s2 a un D para luego convertirlo en un r (esto me da duda, pero lo tomo de las presentaciones que nos han dado )
+	vcvt.F64.F32 d9,s2
+	vcvt.S32.d64 r2,d9	
+	mov r0,r2 
 	pop {pc}
 @Comienzo subrutina norm
+
 norm:
 push {lr}
 	
